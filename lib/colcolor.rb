e12baa08d1d6c083b550a94e500a838f19b5a264
kyoendo <postagie@gmail.com>
2014-07-14 14:48:48 +0900

    refactor COLORS constant

--- lib/colcolor.rb
+++ lib/colcolor.rb
@@ -1,21 +1,14 @@
 require "colcolor/version"
 
 module Colcolor
-  COLORS = Hash[ %i(black red green yellow blue magenta cyan white clear)
-                 .zip([*30..37, 0])
-                 .map { |k,v| [k, "\e[#{v}m"] }
-                ].freeze
-
-  BGCOLORS = Hash[ %i(bg_black bg_red bg_green bg_yellow
-                        bg_blue bg_magenta bg_cyan bg_white)
-                   .zip(40..47)
-                   .map { |k,v| [k, "\e[#{v}m"] }
-                  ].freeze
-
-  EXTRAS = Hash[ %i(clear bold underline blink reverse)
-                 .zip([0, 1, 4, 5, 7])
-                 .map { |k,v| [k, "\e[#{v}m"] }
-                ].freeze
+  COLORS = Hash[
+    %i( black red green yellow blue magenta cyan white
+        bg_black bg_red bg_green bg_yellow
+        bg_blue bg_magenta bg_cyan bg_white
+        clear bold underline blink reverse )
+      .zip([*30..37, *40..47, 0, 1, 4, 5, 7])
+      .map { |k,v| [k, "\e[#{v}m"] }
+    ].freeze
 
   def colco(*colors, regexp:/\S+/)
     cs = colors.dup
