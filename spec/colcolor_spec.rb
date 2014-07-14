24c21dbcf9ca36311fea394b6a6d0026dd8fef34
kyoendo <postagie@gmail.com>
2014-07-14 14:30:22 +0900

    add an example for irregular pattern

--- spec/colcolor_spec.rb
+++ spec/colcolor_spec.rb
@@ -6,28 +6,39 @@ describe Colcolor do
   end
 
   describe "String#c" do
+    before do
+      @str = "George 18 guitarist"
+    end
+
     context "match words and colors sizes" do
       it "colors each words with given colors" do
-        str = "George 18 guitarist"
         expect = "\e[32mGeorge\e[0m \e[33m18\e[0m \e[31mguitarist\e[0m"
         color_set = [:green, :yellow, :red]
-        expect(str.colco(*color_set)).to eq expect
+        expect(@str.colco(*color_set)).to eq expect
       end
     end
 
     context "short colors" do
       it "colors a word white for missing color" do
-        str = "George 18 guitarist"
         expect = "\e[32mGeorge\e[0m \e[33m18\e[0m \e[37mguitarist\e[0m"
         color_set = [:green, :yellow]
-        expect(str.colco(*color_set)).to eq expect
+        expect(@str.colco(*color_set)).to eq expect
       end
 
       it "colors a word white at intermidiate" do
-        str = "George 18 guitarist"
         expect = "\e[32mGeorge\e[0m \e[37m18\e[0m \e[31mguitarist\e[0m"
         color_set = [:green, nil, :red]
-        expect(str.colco(*color_set)).to eq expect
+        expect(@str.colco(*color_set)).to eq expect
+      end
+    end
+
+    context "irregular pattern" do
+      it "gets regexp to define the chunks" do
+        str = "George + 18 guitarist"
+        expect = "\e[32mGeorge\e[0m \e[33m+ 18\e[0m \e[31mguitarist\e[0m"
+        color_set = [:green, :yellow, :red]
+        re = /\+\s*\d+|\S+/
+        expect(str.colco(*color_set, regexp:re)).to eq expect
       end
     end
   end
