546c259f82d032be18153e2ba735b81f2ceccb23
kyoendo <postagie@gmail.com>
2014-07-14 10:30:29 +0900

    first implement String#c

--- lib/colcolor.rb
+++ lib/colcolor.rb
@@ -1,5 +1,34 @@
 require "colcolor/version"
 
 module Colcolor
-  # Your code goes here...
+  COLORS = %i(black red green yellow blue magenta cyan white clear).zip([*30..37, 0]).map { |k,v| [k, "\e[#{v}m"] }.to_h.freeze
+
+  BGCOLORS = %i(bg_black bg_red bg_green bg_yellow bg_blue bg_magenta bg_cyan bg_white).zip(40..47).map { |k,v| [k, "\e[#{v}m"] }.to_h.freeze
+
+  EXTRAS = %i(clear bold underline blink reverse).zip([0, 1, 4, 5, 7]).map { |k,v| [k, "\e[#{v}m"] }.to_h.freeze
+
+  def c(*colors, regexp:/\S+/)
+    cs = colors.dup
+    self.gsub(regexp) do
+      color = COLORS[cs.shift] || COLORS[:white]
+      color + $& + COLORS[:clear]
+    end
+  end
 end
+
+String.send(:include, Colcolor)
+
+__END__
+
+COLOR_MAP = %i(red green yellow).zip(31..33).map { |k,v| [k, "\e[#{v}m"] }.to_h
+
+def color(lines, colors, regexp=/\S+/)
+ lines.each_line.map do |line|
+   cs = colors.dup
+   line.gsub(regexp) do
+     color = COLOR_MAP[cs.shift] || "\e[0m"
+     color + $& + "\e[0m"
+   end
+ end.join
+end
+
--- spec/colcolor_spec.rb
+++ spec/colcolor_spec.rb
@@ -5,7 +5,12 @@ describe Colcolor do
     expect(Colcolor::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context "String#c" do
+    it "returns a colored string" do
+      str = "George 18 guitarist"
+      expect = "\e[32mGeorge\e[0m \e[33m18\e[0m \e[31mguitarist\e[0m"
+      color_set = [:green, :yellow, :red]
+      expect(str.c(*color_set)).to eq expect
+    end
   end
 end
